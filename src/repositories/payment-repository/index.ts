10b8f709f6851e350e651f08df08e5670a86caf8
gacdutra <ga.acdutra@gmail.com>
2023-01-12 21:40:40 -0300

    refactor: add transaction on payment process

--- src/repositories/payment-repository/index.ts
+++ src/repositories/payment-repository/index.ts
@@ -1,24 +1,36 @@
 import { prisma } from "@/config";
-import { Payment } from "@prisma/client";
+import { Payment, TicketStatus } from "@prisma/client";
 
 async function findPaymentByTicketId(ticketId: number) {
   return prisma.payment.findFirst({
     where: {
       ticketId,
-    }
+    },
   });
 }
 
 async function createPayment(ticketId: number, params: PaymentParams) {
-  return prisma.payment.create({
-    data: {
-      ticketId,
-      ...params,
-    }
+  let payment;
+  await prisma.$transaction(async (prisma) => {
+    payment = await prisma.payment.create({
+      data: {
+        ticketId,
+        ...params,
+      },
+    });
+    await prisma.ticket.update({
+      where: {
+        id: ticketId,
+      },
+      data: {
+        status: TicketStatus.PAID,
+      },
+    });
   });
+  return payment;
 }
 
-export type PaymentParams = Omit<Payment, "id" | "createdAt" | "updatedAt">
+export type PaymentParams = Omit<Payment, "id" | "createdAt" | "updatedAt">;
 
 const paymentRepository = {
   findPaymentByTicketId,
